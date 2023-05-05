e937b3c297bd9910bdee75f283aa220c879f31c7
Preet Panchal <preet2020@icloud.com>
2023-05-04 23:36:01 -0400

    main

---
+++ pages/about-us.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+const AboutPage = () => {
+  return (
+    <div className="bg-white py-8">
+      <div className="container mx-auto px-4">
+        <h1 className="text-4xl font-bold text-gray-800 mb-8">About Us</h1>
+        <p className="text-gray-700 mb-6">
+          Our Event-app is dedicated to bringing you the latest information on
+          the hottest events in Ottawa, Saskatchewan, Vancouver, and beyond. We
+          believe that everyone should have access to information about the best
+          events happening in their city, and that's why we created this app.
+        </p>
+        <p className="text-gray-700 mb-6">
+          Our team is made up of passionate event-goers who want to share their
+          love for live music, food festivals, and cultural celebrations with
+          others. We believe that events have the power to bring people together
+          and create unforgettable memories.
+        </p>
+        <p className="text-gray-700 mb-6">
+          Our app is designed to be easy to use and navigate, with up-to-date
+          information on events happening near you. We also offer personalized
+          recommendations based on your interests and preferences, so you never
+          miss an event you'll love.
+        </p>
+        <p className="text-gray-700 mb-6">
+          Thank you for using our Event-app. We hope it brings you joy and
+          excitement as you explore the vibrant event scenes across Canada.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default AboutPage;
